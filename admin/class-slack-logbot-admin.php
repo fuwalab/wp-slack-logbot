afb59e3193fa05576364f18989bd849d5bde0829
ryotsun <ryo.tsugawa@gmail.com>
2018-10-23 22:37:15 +0900

    fix codes following php coding standard

--- admin/class-slack-logbot-admin.php
+++ admin/class-slack-logbot-admin.php
@@ -1,26 +1,44 @@
 <?php
+/**
+ * Slack_Logbot_Admin class
+ *
+ * @package WordPress
+ * @subpackage WP Slack Logbot
+ * @since 1.0.0
+ * @version 1.0.0
+ */
 
-class SlackLogbotAdmin {
+/**
+ * Class Slack_Logbot_Admin
+ */
+class Slack_Logbot_Admin {
 
-	function __construct()
-	{
-		add_action('admin_menu', array( $this, 'create_admin_menu' ));
+	/**
+	 * Slack_Logbot_Admin constructor.
+	 */
+	function __construct() {
+		add_action( 'admin_menu', array( $this, 'create_admin_menu' ) );
 	}
 
-	public function create_admin_menu()
-	{
-		add_menu_page('WP Slack Logbot Settings', 'WP Slack Logbot', 'administrator',
-			__FILE__, array( $this, 'show_page' ), plugins_url('/images/icon.png', __FILE__ ));
+	/**
+	 * Create admin menu.
+	 */
+	public function create_admin_menu() {
+		add_menu_page( 'WP Slack Logbot Settings', 'WP Slack Logbot', 'administrator', __FILE__, array( $this, 'show_page' ), plugins_url( '/images/icon.png', __FILE__ ) );
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
 	}
 
-	public function register_settings()
-	{
+	/**
+	 * Register settings.
+	 */
+	public function register_settings() {
 		register_setting( 'wp-slack-logbot-settings-group', 'wp-slack-logbot-bot-user-oauth-access-token' );
 	}
 
-	public function show_page()
-	{
+	/**
+	 * Show setting page.
+	 */
+	public function show_page() {
 		?>
 		<div class="wrap">
 			<h2>WP Slack Logbot</h2>
@@ -28,7 +46,7 @@ class SlackLogbotAdmin {
 				<table class="form-table">
 					<tr valign="top">
 						<th scope="row">Bot User OAuth Access Token</th>
-						<td><input type="text" name="wp-slack-logbot-bot-user-oauth-access-token" value="<?php echo esc_attr( get_option('wp-slack-logbot-bot-user-oauth-access-token') ); ?>"></td>
+						<td><input type="text" name="wp-slack-logbot-bot-user-oauth-access-token" value="<?php echo esc_attr( get_option( 'wp-slack-logbot-bot-user-oauth-access-token' ) ); ?>"></td>
 					</tr>
 				</table>
 				<?php settings_fields( 'wp-slack-logbot-settings-group' ); ?>
--- class-wp-slack-logbot.php
+++ class-wp-slack-logbot.php
@@ -18,29 +18,50 @@
 require_once 'includes/class-slack-logbot.php';
 require_once 'admin/class-slack-logbot-admin.php';
 
+/**
+ * Class WP_Slack_Logbot
+ */
 class WP_Slack_Logbot {
 	const TABLE_NAME = 'slack_logbot';
+
+	/**
+	 * Logbot version
+	 *
+	 * @var string $slack_logbot_version
+	 */
 	var $slack_logbot_version = '1.0';
+
+	/**
+	 * Admin instance
+	 *
+	 * @var Slack_Logbot_Admin $admin
+	 */
 	var $admin;
 
-	function __construct()
-	{
-		new SlackLogbot();
+	/**
+	 * WP_Slack_Logbot constructor.
+	 */
+	function __construct() {
+		new Slack_Logbot();
 		$this->register_activation_hook();
 
-		// enable admin
-		$this->admin = new SlackLogbotAdmin();
+		// enable admin.
+		$this->admin = new Slack_Logbot_Admin();
 	}
 
-	public function register_activation_hook()
-	{
-		register_activation_hook( __FILE__, array( $this, 'install') );
+	/**
+	 * Register activation hook.
+	 */
+	public function register_activation_hook() {
+		register_activation_hook( __FILE__, array( $this, 'install' ) );
 	}
 
-	public function install()
-	{
+	/**
+	 * Create table for this plugin.
+	 */
+	public function install() {
 		global $wpdb;
-		$table_name = $wpdb->prefix . self::TABLE_NAME;
+		$table_name      = $wpdb->prefix . self::TABLE_NAME;
 		$charset_collate = $wpdb->get_charset_collate();
 
 		$sql = "CREATE TABLE $table_name (

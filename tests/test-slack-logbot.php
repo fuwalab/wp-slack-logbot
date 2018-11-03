d3c9e07bc646ac04da5fbd96301c634766ed41db
ryotsun <ryo.tsugawa@gmail.com>
2018-11-04 00:55:44 +0900

    add test code

--- tests/test-slack-logbot.php
+++ tests/test-slack-logbot.php
@@ -51,18 +51,14 @@ class Slack_Logbot_Test extends WP_UnitTestCase {
 	 *
 	 * @since 1.0.0
 	 */
-	function test_if_exists_record() {
+	function test_save() {
 		global $wpdb;
 
 		$slack_logbot = $this->slack_logbot;
 		$table_name   = $wpdb->prefix . $slack_logbot::TABLE_NAME;
 
-		$json_string = '{"authed_users": ["XXX1234", "MMEOAD345"], "token": "XXXabckekejlasdgKSH", "team_id": "X22345O", "type": "event_callback", "event": {"ts": "1539570604.000100", "client_msg_id": "06e8b044-e420-4879-bf44-3f762dc8eecf", "user": "UADJETKV9", "event_ts": "1539570604.000100", "type": "message", "channel": "CDFBSHT46", "text": "dummy message.", "channel_type": "channel"}, "event_time": 1539570604, "api_app_id": "ACXXXJEA1", "event_id": "EvDD486j8"}';
-		$data_array  = json_decode( $json_string, true );
-		$data        = $slack_logbot->prepare_data( $data_array );
-		$result      = $wpdb->insert( $table_name, $data );
-
-		$this->assertSame( $result, 1 );
+		$data = $this->get_slack_post();
+		$slack_logbot->save( $data );
 
 		$result = $wpdb->get_row( "select * from $table_name limit 1" );
 
@@ -83,6 +79,69 @@ class Slack_Logbot_Test extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test saving wp_post.
+	 *
+	 * @throws ReflectionException Reflection exception.
+	 * @since 1.0.0
+	 */
+	function test_save_post() {
+		global $wpdb;
+
+		$reflection = new \ReflectionClass( $this->slack_logbot );
+
+		// Make category first.
+		$method = $reflection->getMethod( 'get_category' );
+		$method->setAccessible( true );
+		$parent_category    = $method->invoke( $this->slack_logbot, 'Sample Team' );
+		$parent_category_id = wp_insert_category( $parent_category );
+		$category           = $method->invoke( $this->slack_logbot, 'sample channel', 0, $parent_category_id );
+		$category_id        = wp_insert_category( $category );
+
+		$this->assertTrue( $parent_category_id > 0 );
+		$this->assertTrue( $category_id > 0 );
+
+		// Prepare wp post.
+		$wp_user_id = get_current_user_id() > 0 ? get_current_user() : 1;
+		$post       = array(
+			'ID'            => 0,
+			'post_title'    => '[Slack Logbot] sample channel(2018-10-15)',
+			'post_content'  => 'sample post message.',
+			'post_status'   => 'publish',
+			'post_author'   => $wp_user_id,
+			'post_category' => array( $parent_category_id, $category_id ),
+		);
+
+		// Save wp post.
+		$method = $reflection->getMethod( 'save_wp_post' );
+		$method->setAccessible( true );
+		$method->invoke( $this->slack_logbot, $post );
+
+		$table_name = $wpdb->prefix . 'posts';
+		$result     = $wpdb->get_results( "select * from $table_name", ARRAY_A );
+
+		$this->assertSame( count( $result ), 1 );
+		$this->assertSame( $result[0]['post_title'], '[Slack Logbot] sample channel(2018-10-15)' );
+		$this->assertSame( $result[0]['post_content'], 'sample post message.' );
+	}
+
+	/**
+	 * Test if json decoded array from slack returns correct array data.
+	 *
+	 * @since 1.0.0
+	 */
+	function test_prepare_data() {
+		$slack_logbot = $this->slack_logbot;
+
+		$json_string = '{"authed_users": ["XXX1234", "MMEOAD345"], "token": "XXXabckekejlasdgKSH", "team_id": "X22345O", "type": "event_callback", "event": {"ts": "1539570604.000100", "client_msg_id": "06e8b044-e420-4879-bf44-3f762dc8eecf", "user": "UADJETKV9", "event_ts": "1539570604.000100", "type": "message", "channel": "CDFBSHT46", "text": "dummy message.", "channel_type": "channel"}, "event_time": 1539570604, "api_app_id": "ACXXXJEA1", "event_id": "EvDD486j8"}';
+		$data_array  = json_decode( $json_string, true );
+		$actual_data = $slack_logbot->prepare_data( $data_array );
+
+		$expected_data = $this->get_slack_post();
+
+		$this->assertEquals( $expected_data, $actual_data );
+	}
+
+	/**
 	 * Create required table.
 	 *
 	 * @since 1.0.0
@@ -90,5 +149,29 @@ class Slack_Logbot_Test extends WP_UnitTestCase {
 	private function create_table() {
 		$wp_slack_logbot = new WP_Slack_Logbot();
 		$wp_slack_logbot->install();
+	}
+
+	/**
+	 * Get sample slack post.
+	 *
+	 * @since 1.0.0
+	 * @return array slack post
+	 */
+	private function get_slack_post() {
+		return array(
+			'team_id'             => 'X22345O',
+			'type'                => 'event_callback',
+			'api_app_id'          => 'ACXXXJEA1',
+			'event_id'            => 'EvDD486j8',
+			'event_user'          => 'UADJETKV9',
+			'event_client_msg_id' => '06e8b044-e420-4879-bf44-3f762dc8eecf',
+			'event_type'          => 'message',
+			'event_text'          => 'dummy message.',
+			'event_channel'       => 'CDFBSHT46',
+			'event_channel_type'  => 'channel',
+			'event_time'          => 1539570604,
+			'event_datetime'      => '2018-10-15 02:30:04',
+			'create_date'         => '2018-10-15',
+		);
 	}
 }
